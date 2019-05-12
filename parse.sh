a396abb6f18094983ea6225d740dfe8da7b62f59
Patrice Lacroix <patrice.lacroix@stamp-it.io>
2019-05-11 21:37:11 -0400

    Parsing functions with a few tests.

---
+++ parse.sh
@@ -0,0 +1,26 @@
+# Returns the user@host:port part of a jumpuser@jumphost:jumpport,user@host:port specification
+function get_remote_spec () {
+	local access_spec
+	access_spec=$1 					# e.g. jumpuser@jumphost:jumpport,user@host:port
+
+	if [ -z "$access_spec" ]; then
+		echo "$0/get_remote_spec: empty access_spec" >&2
+		return 1
+	fi
+
+	echo "$access_spec" | sed -n "s/^.*,\([^,]*\)$/\1/; p"
+}
+
+function get_remote_host () {
+	local access_spec remote_host
+	access_spec=$1 					# e.g. jumpuser@jumphost:jumpport,user@host:port
+
+	remote_host="$(get_remote_spec $access_spec)"
+
+	remote_host="${remote_host#*@}"
+	remote_host="${remote_host%:*}"
+
+	echo "$remote_host"
+}
+
+
