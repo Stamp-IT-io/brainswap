08e6b168f978fab5ee2ca6b7bf13cfd9a61b7911
Patrice Lacroix <patrice.lacroix@stamp-it.io>
2019-05-13 23:37:00 -0400

    Add get_jump_spec and tests

--- parse.sh
+++ parse.sh
@@ -11,6 +11,19 @@ function get_remote_spec () {
 	echo "$access_spec" | sed -n "s/^.*,\([^,]*\)$/\1/; p"
 }
 
+# Returns the jumpuser@jumphost:jumpport part of a jumpuser@jumphost:jumpport,user@host:port specification
+function get_jump_spec () {
+	local access_spec
+	access_spec=$1 					# e.g. jumpuser@jumphost:jumpport,user@host:port
+
+	if [ -z "$access_spec" ]; then
+		echo "$0/get_remote_spec: empty access_spec" >&2
+		return 1
+	fi
+
+	echo "$access_spec" | sed -n "s/^\(.*\),[^,]*$/\1/; p"
+}
+
 function get_remote_host () {
 	local access_spec remote_host
 	access_spec=$1 					# e.g. jumpuser@jumphost:jumpport,user@host:port
