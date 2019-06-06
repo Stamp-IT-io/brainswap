57f67b59ee5600619f352eb659e00a6a47deb75f
Patrice Lacroix <patrice.lacroix@stamp-it.io>
2019-06-05 22:18:58 -0400

    Integration of ssh.sh module into brainswap.

--- ssh.sh
+++ ssh.sh
@@ -19,7 +19,7 @@ function do_ssh () {
 	remote_userhost="$(get_remote_userhost ${access_spec})"
 
 	remote_port="$(get_remote_port ${access_spec})"
-	remote_port_arg="${remote_port:+-J $remote_port}"
+	remote_port_arg="${remote_port:+-p $remote_port}"
 
 	ssh $jump_arg $remote_userhost $remote_port_arg "$@"
 }
